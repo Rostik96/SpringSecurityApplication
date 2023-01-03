8e4aa55c52a461b3b353af030f08db6d1394f5b3
Rost <coderost@yandex.ru>
2023-01-03 22:37:32 +0300

    Create "Person" model.

---
+++ src/main/java/com/study/rost/models/Person.java
@@ -0,0 +1,46 @@
+package com.study.rost.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "person")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов")
+    @Column(name = "username")
+    private String username;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    @Column(name = "password")
+    private String password;
+
+    public Person(String username, int yearOfBirth) {
+        this.username = username;
+        this.yearOfBirth = yearOfBirth;
+    }
+}
