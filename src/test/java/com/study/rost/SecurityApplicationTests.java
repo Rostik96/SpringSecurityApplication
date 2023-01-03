327f3d16c32da1fc8edbe7701b7008bf3ba15c11
Rost <coderost@yandex.ru>
2023-01-03 22:29:00 +0300

    Initial commit

---
+++ src/test/java/com/study/rost/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.rost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
