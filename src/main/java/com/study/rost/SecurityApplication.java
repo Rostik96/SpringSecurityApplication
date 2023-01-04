cf6cbc67345c3a7410f86af0c4d317251ba583cf
Rost <coderost@yandex.ru>
2023-01-04 21:56:18 +0300

    Checkstyle refactoring of SecurityApplication class.

--- src/main/java/com/study/rost/SecurityApplication.java
+++ src/main/java/com/study/rost/SecurityApplication.java
@@ -1,11 +1,7 @@
 package com.study.rost;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.study.rost.services.PeopleService;
-import lombok.AllArgsConstructor;
 
 @SpringBootApplication
 public class SecurityApplication {
