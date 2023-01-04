46ab6ef2cb62c2a320953991e18b9c1c4a53de23
Rost <coderost@yandex.ru>
2023-01-04 14:43:21 +0300

    Create wrapper-class over the Person to uniformity of obtaining principal's information.

---
+++ src/main/java/com/study/rost/security/PersonDetails.java
@@ -0,0 +1,63 @@
+package com.study.rost.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.study.rost.models.Person;
+import lombok.AllArgsConstructor;
+
+/**
+ * Класс-обёртка над аутентифицируемой сущностью для единообразия получения её учётных данных и
+ * системной информации по этим credentials.
+ */
+
+@AllArgsConstructor
+public class PersonDetails implements UserDetails {
+    private final Person person;
+
+    /**
+     * Необходимо для получения аутентифицированного пользователя.
+     *
+     * @return principal
+     */
+    public Person getPerson() {
+        return person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return person.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
