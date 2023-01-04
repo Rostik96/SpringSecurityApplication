a1f1b49c2a6c8816acb4395c0f3775569be377ff
Rost <coderost@yandex.ru>
2023-01-04 14:46:03 +0300

    Configure SpringSecurity via our AuthenticationProvider implementation.

--- src/main/java/com/study/rost/config/SecurityConfig.java
+++ src/main/java/com/study/rost/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.study.rost.config;
 
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter { //Главный класс, где мы настраиваем Spring Security
+    private final AuthenticationProvider authenticationProviderImpl;
+
     //Настраивает аутентификацию
     @Override
     protected void configure(AuthenticationManagerBuilder authentication) {
-        authentication.authenticationProvider(null); //TODO have to autowire {@link AuthenticationProvider}
+        authentication.authenticationProvider(authenticationProviderImpl); //TODO have to autowire {@link AuthenticationProvider}
     }
 }
