1750daebe52163698d80fa1eb5611105fd06d81a
Rost <coderost@yandex.ru>
2023-01-04 17:29:41 +0300

    Add .showUserInfo() in HelloController with corresponding HTML-view.

--- src/main/java/com/study/rost/controllers/HelloController.java
+++ src/main/java/com/study/rost/controllers/HelloController.java
@@ -1,12 +1,25 @@
 package com.study.rost.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.study.rost.security.PersonDetails;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
         return "hello";
+    }
+
+    @GetMapping("/show-user-info")
+    public ModelAndView showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+        return new ModelAndView("userInfo", "principal", personDetails.getPerson());
     }
 }
