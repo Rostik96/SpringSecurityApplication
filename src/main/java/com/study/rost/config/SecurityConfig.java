46dd6a5829f3d2c9fd9d9438fdfa4ab1b13c5866
Rost <coderost@yandex.ru>
2023-01-04 20:13:29 +0300

    Refactor Spring Security configuration via custom login page and corresponding HttpSecurity settings.

--- src/main/java/com/study/rost/config/SecurityConfig.java
+++ src/main/java/com/study/rost/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.study.rost.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -13,16 +14,26 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    //private final AuthenticationProvider authenticationProviderImpl;
-
-    //Настраивает аутентификацию
-    /*@Override
-    protected void configure(AuthenticationManagerBuilder authentication) {
-        authentication.authenticationProvider(authenticationProviderImpl);
-    }*/
-
     private final PersonDetailsService personDetailsService;
 
+    /**
+     * В данном методе конфигурируем сам Spring Security & авторизацию.
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/auth/login", "/error").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/auth/login")
+                .loginProcessingUrl("/process_login") //Security будет ждать логин/пароль по данному адресу.
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    //Настраиваем логику аутентификацию
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
---
+++ src/main/java/com/study/rost/controllers/AuthenticationController.java
@@ -0,0 +1,14 @@
+package com.study.rost.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthenticationController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+}
