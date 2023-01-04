3b4c5bb60b97bc2297042edd4c42e068fba44694
Rost <coderost@yandex.ru>
2023-01-04 15:07:49 +0300

    Create HelloController with its html-view.

---
+++ src/main/java/com/study/rost/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.study.rost.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
