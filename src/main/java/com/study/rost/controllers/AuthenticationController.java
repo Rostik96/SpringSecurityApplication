3711a7bdb8e084579de2d80f513928ea5658849b
Rost <coderost@yandex.ru>
2023-01-07 20:38:49 +0300

    Add ModelMapper & JwtUtil objs into AuthenticationController. Refactor corresponding methods via them.

--- src/main/java/com/study/rost/controllers/AuthenticationController.java
+++ src/main/java/com/study/rost/controllers/AuthenticationController.java
@@ -1,23 +1,33 @@
 package com.study.rost.controllers;
 
+import java.util.Map;
+
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.study.rost.dto.PersonDTO;
 import com.study.rost.models.Person;
 import com.study.rost.services.RegistrationService;
+import com.study.rost.util.JwtUtil;
 import com.study.rost.util.PersonValidator;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+    private final JwtUtil jwtUtil;
+    private final ModelMapper modelMapper;
     private final RegistrationService registrationService;
     private final PersonValidator personValidator;
     @GetMapping("/login")
@@ -31,12 +41,18 @@ public class AuthenticationController {
     }
 
     @PostMapping("/registration")
-    public String registration(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+    public Map<String, String> registration(@RequestBody PersonDTO personDTO, BindingResult bindingResult) {
+        Person person = convertToPerson(personDTO);
         personValidator.validate(person, bindingResult);
         if(bindingResult.hasErrors())
-            return "/auth/registration";
+            return Map.of("message", "Error!"); //TODO: добавить @ExceptionHandler метод
 
         registrationService.register(person);
-        return "redirect:/auth/login";
+        String token = jwtUtil.generateToken(person.getUsername());
+        return Map.of("jwt-token", token);
+    }
+
+    private Person convertToPerson(PersonDTO personDTO) {
+        return modelMapper.map(personDTO, Person.class);
     }
 }
