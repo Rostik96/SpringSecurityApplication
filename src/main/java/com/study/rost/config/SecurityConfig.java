1e116131e7ad9dbf4d9f6d9d15184c88da7eb241
Rost <coderost@yandex.ru>
2023-01-07 19:21:38 +0300

    Refactor config with taking into account added roles("Role" enum)

--- src/main/java/com/study/rost/config/SecurityConfig.java
+++ src/main/java/com/study/rost/config/SecurityConfig.java
@@ -1,20 +1,22 @@
 package com.study.rost.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.study.rost.services.PersonDetailsService;
 import lombok.AllArgsConstructor;
 
+import static com.study.rost.security.Role.*;
+
 @AllArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final PersonDetailsService personDetailsService;
 
@@ -25,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                     .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
-                    .anyRequest().authenticated()
+                    .anyRequest().hasAnyRole(ROLE_ADMIN.roleName(), ROLE_USER.roleName())
                 .and()
                     .formLogin()
                     .loginPage("/auth/login")
